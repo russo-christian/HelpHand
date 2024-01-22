6dc3f9e76af1e4f2f542bf5c51c867a244909096
Asma <AsmaStudyIT@gmail.com>
2024-01-22 14:10:09 +0700

    add public and view

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routers/user.router');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routers/user.router");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB TODO: move db to .env file
-mongoose.connect('mongodb://localhost:27017/helphanddb').then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/helphanddb")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
+
+// Serve the login page
+app.get("/login", (req, res) => {
+  // You can render your login page here or serve an HTML file
+  res.sendFile(__dirname + "/views/login.html");
+});
 
 // Start the server
 app.listen(port, () => {
