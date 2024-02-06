245a055b8ea1009e1317289e01c1252ab3ee9219
russo-christian <russo-christian@proton.me>
2024-02-06 12:08:02 +1100

    update task and user testing, add server testing

--- test/task.test.js
+++ test/task.test.js
@@ -1,23 +1,29 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../server');
 const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const server = require('../server'); // Adjust this to your server file's path
 
 chai.use(chaiHttp);
 
-describe('Task API Tests', () => {
+describe('Task API Tests', function() {
+    this.timeout(10000); // Set the timeout for all tests in this suite
     let createdTaskId = null;
 
     // Test for creating a new task
-    describe('POST /api/tasks', () => {
-        it('should create a new task and return the task data', (done) => {
+    describe('POST /api/tasks', function() {
+        it('should create a new task and return the task data', function(done) {
             const testTaskData = {
                 posterId: 'Test task posterId',
                 datePosted: '1900-01-01',
-                description: 'Test task description',
-                pay: '$10.00'
+                title: 'Test Task Title',
+                description: 'Test Task Description',
+                dueDate: '2050-01-01',
+                pay: 10,
+                location: 'Test Task Location',
+                category: 'Test Task Category',
+                imagePath: '/path'
             };
-    
+
             chai.request(server)
                 .post('/api/tasks')
                 .send(testTaskData)
@@ -25,19 +31,17 @@ describe('Task API Tests', () => {
                     expect(res).to.have.status(201);
                     expect(res.body).to.be.an('object');
                     expect(res.body).to.have.property('_id');
-                    createdTaskId = res.body._id;
+                    createdTaskId = res.body._id; // Save the created task ID for further tests
                     expect(res.body).to.have.property('posterId', testTaskData.posterId);
                     expect(res.body).to.have.property('datePosted', '1900-01-01T00:00:00.000Z');
-                    expect(res.body).to.have.property('description', testTaskData.description);
-                    expect(res.body).to.have.property('pay', testTaskData.pay);
                     done();
                 });
         });
     });
 
     // Test for retrieving all tasks
-    describe('GET /api/tasks', () => {
-        it('should get all tasks', (done) => {
+    describe('GET /api/tasks', function() {
+        it('should get all tasks', function(done) {
             chai.request(server)
                 .get('/api/tasks')
                 .end((err, res) => {
@@ -49,8 +53,8 @@ describe('Task API Tests', () => {
     });
 
     // Test for task id search
-    describe('GET /api/tasks/:id', () => {
-        it('should retrieve a task by id', (done) => {
+    describe('GET /api/tasks/:id', function() {
+        it('should retrieve a task by id', function(done) {
             chai.request(server)
                 .get(`/api/tasks/${createdTaskId}`)
                 .end((err, res) => {
@@ -61,42 +65,42 @@ describe('Task API Tests', () => {
                 });
         });
     });
-    
+
     // Test for task update
-    describe('PUT /api/tasks/:id', () => {
-      it('should update a task by id', (done) => {
-          const updatedTaskData = {
-              description: 'Updated task description',
-              pay: '$20.00'
-          };
-  
-          chai.request(server)
-              .put(`/api/tasks/${createdTaskId}`)
-              .send(updatedTaskData)
-              .end((err, res) => {
-                  expect(res).to.have.status(200);
-                  expect(res.body).to.be.an('object');
-                  expect(res.body).to.have.property('_id', createdTaskId);
-                  expect(res.body).to.have.property('description', updatedTaskData.description);
-                  expect(res.body).to.have.property('pay', updatedTaskData.pay);
-                  done();
-              });
-      });
-  });
+    describe('PUT /api/tasks/:id', function() {
+        it('should update a task by id', function(done) {
+            const updatedTaskData = {
+                description: 'Updated task description',
+                pay: 20
+            };
+
+            chai.request(server)
+                .put(`/api/tasks/${createdTaskId}`)
+                .send(updatedTaskData)
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body).to.have.property('_id', createdTaskId);
+                    expect(res.body).to.have.property('description', updatedTaskData.description);
+                    expect(res.body).to.have.property('pay', updatedTaskData.pay);
+                    done();
+                });
+        });
+    });
 
     // Test for task delete
-    describe('DELETE /api/tasks/:id', () => {
-      it('should delete a task by id', (done) => {
-          chai.request(server)
-              .delete(`/api/tasks/${createdTaskId}`)
-              .end((err, res) => {
-                  expect(res).to.have.status(200);
-                  expect(res.body).to.be.an('object');
-                  expect(res.body).to.have.property('_id', createdTaskId);
-                  chai.request(server).get(`/api/tasks/${createdTaskId}`).end(/* ... */);
-                  done();
-              });
-      });
-  });
+    describe('DELETE /api/tasks/:id', function() {
+        it('should delete a task by id', function(done) {
+            chai.request(server)
+                .delete(`/api/tasks/${createdTaskId}`)
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('object');
+                    expect(res.body).to.have.property('_id', createdTaskId);
+                    // Optionally, verify deletion with another GET request
+                    done();
+                });
+        });
+    });
 
 });
