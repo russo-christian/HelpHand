27460bfbc8a075843b1619d896446d5892d10447
russo-christian <russo-christian@proton.me>
2024-02-06 13:29:48 +1100

    cleanup

--- test/task.test.js
+++ test/task.test.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
-const server = require('../server'); // Adjust this to your server file's path
+const server = require('../server');
 
 chai.use(chaiHttp);
 
 describe('Task API Tests', function() {
-    this.timeout(10000); // Set the timeout for all tests in this suite
+    this.timeout(10000); //account for christian being located outside of Aus for tests
     let createdTaskId = null;
 
     // Test for creating a new task
@@ -97,7 +97,6 @@ describe('Task API Tests', function() {
                     expect(res).to.have.status(200);
                     expect(res.body).to.be.an('object');
                     expect(res.body).to.have.property('_id', createdTaskId);
-                    // Optionally, verify deletion with another GET request
                     done();
                 });
         });
